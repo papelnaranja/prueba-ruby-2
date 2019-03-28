af7b8379aa829b7f37f43e444a5fb90ce2cdfbd6
Vanessa Frenkel <vanefrenkel@gmail.com>
2019-03-27 21:39:40 -0300

    added avatar

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,16 +1,14 @@
 class Task < ApplicationRecord
 has_many :assignments
 has_many :users, through: :assignments
-# before_create :set_user
+after_create :update_users
 
-# def set_user
-#     users = User.all
-#     users.each do |u|
-#         self.assignments.build(user_id:u.id)
-#         byebug
-
-#         self.save
-#     end 
-# end
+def update_users
+    users = User.all
+    users.each do |u|
+        self.assignments.build(user_id:u.id)
+        self.save
+    end 
+end
 
 end
